7b8c3bf8915941694bf59a4d4bc8c43a4133f15e
Ishika Rawat <ishikarwt123@gmail.com>
2026-06-10 23:15:07 +0530

    Update auth.py

--- cognisafe-backend/schemas/auth.py
+++ cognisafe-backend/schemas/auth.py
@@ -10,6 +10,7 @@ class RegisterRequest(BaseModel):
 class LoginRequest(BaseModel):
     email: str
     password: str
+
 class TokenResponse(BaseModel):
     access_token: str
     token_type: str = "bearer"
